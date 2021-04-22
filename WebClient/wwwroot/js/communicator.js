cc85870c7926d2c152487d1821c7ab5e0d3a9a74
Isabel Wan <isabelwan@Isabels-MacBook-Pro.local>
2021-04-22 11:38:45 -0400

    added dummy communicator

---
+++ WebClient/wwwroot/js/communicator.js
@@ -0,0 +1,14 @@
+﻿
+var communicator = {
+    publish: function (topic, message) {
+        console.log("Client called publish method");
+    }
+
+    subscribe: function (topic) {
+        console.log("Client called subscribe method");
+    }
+
+    unsubscribe: function (topic) {
+        console.log("Client called unsubscribe method");
+    }
+}
