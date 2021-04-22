d6de92f7eb212bfe92a07229c7d7af561256c05a
Ivy Shi <jia.chen.shi@duke.edu>
2021-04-22 16:06:24 -0400

    added webclient ui & js code

--- WebClient/wwwroot/js/communicator.js
+++ WebClient/wwwroot/js/communicator.js
@@ -1,14 +1,27 @@
 ﻿
-var communicator = {
-    publish: function (topic, message) {
+function communicator() {
+
+    this.publish = function (topic, message) {
         console.log("Client called publish method");
     }
 
-    subscribe: function (topic) {
+    this.subscribe = function (topic) {
         console.log("Client called subscribe method");
     }
 
-    unsubscribe: function (topic) {
+    this.unsubscribe = function (topic) {
         console.log("Client called unsubscribe method");
     }
+}
+
+function subscribeTest() {
+    console.log("Client called subscribe method");
+}
+
+function publishTest() {
+    console.log("Client called publish method");
+}
+
+function unsubscribeTest() {
+    console.log("Client called unsubscribe method");
 }
