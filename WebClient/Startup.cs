5bba2fc55ce2f81cd379a9dd9064837776efb99e
Isabel Wan <isabelwan@Isabels-MacBook-Pro.local>
2021-04-22 23:06:37 -0400

    enabled client to publish message via communicator, added signalR service

--- WebClient/Startup.cs
+++ WebClient/Startup.cs
@@ -24,6 +24,7 @@ namespace WebClient
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- WebClient/wwwroot/js/site.js
+++ WebClient/wwwroot/js/site.js
@@ -1,13 +1,20 @@
-﻿
+﻿"use strict";
+
+import Communicator from "./communicator.js";
+
+let comm = new Communicator();
+
+comm.connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
 
 document.getElementById("subscribeButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var topic = document.getElementById("topicInput").value;
-
-    //let communicatorInstance = new communicator(null);
-    //communicatorInstance.subscribe();
-
-   // subscribeTest();
 
 
 });
@@ -17,14 +24,12 @@ document.getElementById("publishButton").addEventListener("click", function (eve
     var topic = document.getElementById("topicInput").value;
     var message = document.getElementById("messageInput").value;
 
-    //publishTest();
+    comm.publish(user, message);
 
 });
 
 document.getElementById("unsubscribeButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var topic = document.getElementById("topicInput").value;
-
-    //unsubscribeTest();
 
 });
