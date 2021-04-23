0c899421a8a606317a0c7cb977a72a0d5ffc246f
Ivy Shi <jia.chen.shi@duke.edu>
2021-04-23 00:42:23 -0400

    implemented subscribe/unsubscribte and publish to a topic

--- CorEstafette/Hubs/TestHub.cs
+++ CorEstafette/Hubs/TestHub.cs
@@ -11,8 +11,26 @@ namespace CorEstafette.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
         //method for client to subscribe for a topic
+        public async Task SubscribeTopic(string user, string topic)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, topic);
+            await Clients.Group(topic).SendAsync("ReceiveGroup", $"{Context.ConnectionId} has joined the group {topic}.");
+        }
+
         //method for client to unsubscribe from a topic
+        public async Task UnsubscribeTopic(string user, string topic)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, topic);
+            await Clients.Group(topic).SendAsync("ReceiveGroup", $"{Context.ConnectionId} has lefted the group {topic}.");
+
+        }
+
         //method for client to publish a message under a topic
+        public async Task PublishMessage(string user, string topic, string message) //can be called by a connected client
+        {
+            await Clients.Group(topic).SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
