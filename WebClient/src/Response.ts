1486acb24b230b0c696301ca03d4eece7b45ed21
Ivy Shi <jia.chen.shi@duke.edu>
2021-04-28 21:00:04 -0400

    Added message class and interface

--- WebClient/src/Response.ts
+++ WebClient/src/Response.ts
@@ -1,16 +1,18 @@
 ﻿import { IMessage } from "./IMessage";
 
-export class Message implements IMessage {
+export class Response implements IMessage {
     correlationId: string;
     content: string;
     sender: string;
     topic: string;
+    success: boolean;
 
-    constructor(id: string, content: string, sender: string, topic: string) {
+    constructor(_id: string, _content: string, sender: string, topic: string, success: boolean) {
         this.correlationId = id;
         this.content = content;
         this.sender = sender;
         this.topic = topic;
+        this.success = success;
     }
 
 }
