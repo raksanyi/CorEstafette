a9c495dd88d0e503e28505e79772a87ebebff704
Edith Desmarais <edith.d.desmarais@gmail.com>
2021-05-04 14:30:57 -0400

    Added a Contructor to Response Added a Dictionnary to Hub to keep track of username and connection id on connection start Added an overload of OnDisconnectAsync to remove user from the repertory.

--- CorEstafette/Hubs/SignalRHub.cs
+++ CorEstafette/Hubs/SignalRHub.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 using SignalRCommunicator;
+using System.Collections.Concurrent;
 
 //Hub manages connection, group, messaging
 namespace CorEstafette.Hubs
 {
     public class SignalRHub : Hub
     {
+        private static ConcurrentDictionary<string, string> UserConnections = new ConcurrentDictionary<string, string>();
+
+        public async Task<IResponse> ConnectAsync(string userName)
+        {
+            var success = UserConnections.TryAdd(userName, Context.ConnectionId);
+            string content = $"Username {userName} {(success ? "added successfully" : "already exists")} in the repertory.";
+            return new Response(userName, null, content, true);
+        }
         public async Task PublishAsync(Message message)
         {
             await Clients.OthersInGroup(message.Topic).SendAsync("OnPublish", message);
@@ -16,6 +25,9 @@ namespace CorEstafette.Hubs
         public async Task<IResponse> SubscribeTopicAsync(Message message)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, message.Topic);
+            string str = Context.User.Identity.Name;
+            string str2 = Context.UserIdentifier;
+            
             message.Content = $"{message.Sender} successfully subscribed to topic {message.Topic}";
             return new Response(message, true);
         }
@@ -26,6 +38,19 @@ namespace CorEstafette.Hubs
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, message.Topic);
             message.Content = $"{message.Sender} successfully unsubscribe from topic {message.Topic}";
             return new Response(message, true);
+        }
+
+        public override Task OnDisconnectedAsync(System.Exception exception)
+        {
+            string userName = "";
+            foreach( var pair in UserConnections)
+            {
+                if (pair.Value == Context.ConnectionId)
+                    userName = pair.Key;
+            }
+
+            UserConnections.TryRemove(userName, out string connectId);
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
